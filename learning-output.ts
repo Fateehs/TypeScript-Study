970a1c201b089523c86c5c82afec633782a3ba8d
Fateehs <benzer1010@gmail.com>
2023-09-02 17:15:41 +0300

    Getting Started & Installation

---
+++ learning-output.ts
@@ -0,0 +1,18 @@
+// Getting Started & Installation
+// 1-) Node.js kurulumu ve TypeScript kurulumu yapıldı.
+// 2-) npm init --y kullanılarak package.json oluşturuldu.
+// 3-) tsconfig.json dosyası oluşturuldu ve compilerOptions tanımlandı.
+// 4-) compilerOptions için {
+//     "lib": ["ESNext"], ECMAScript ve API'lerinin projeyi kullanmasını ve en son ve en gelişmiş JavaScript özelliklerini kullanılmasına izin verdik.
+//      Kütüphane özellikeri içerisinde "DOM" kullanımı tarayıcı yani HTML desteği ekler.
+//     "target": "ES6", TS kodunu hangi şekilde derleyeceğini ifade eder burada ECMAScript6 için derleyeceğiniz söyledik.
+//     "strict": true, Sıkı bir tür denetimi yapıtık.
+//     "module": "CommonJS", TS formatı için modül sistemimizi CommonJS seçtik
+//     "alwaysStrict": true, JS kodunun her zmaan use strict modunda çalışmasını sağladık
+//     "moduleResolution": "Node", Modülün çalışacağı yeri seçtik
+//     "esModuleInterop": true, ES ile CommonJS modülü arasındaki geçişleri kolaylaştırdık
+//     "outDir": "./dist", Output directory ayarı için
+//     "rootDir": "src", Kaynağı belirledik
+//     "noUnusedParameters": true, Parametre kullanımını zorunlu tutar
+//     "noUnusedLocals": true Eğerki değer yazılır ve kullanılmazsa hata almamızı sağlar
+// } özellikleri girildi 
